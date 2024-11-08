3518cb3713c9cfde13fe77e8bfb2021749fff4f4
Kevin wachira <haizkelvin1@gmail.com>
2024-11-08 21:18:59 +0300

    Create ODD,EVEN.c

---
+++ ODD,EVEN.c
@@ -0,0 +1,17 @@
+//program to check whether number is odd or even
+#include<stdio.h>
+void checkEvenOrOdd(int num) {
+if (num % 2 == 0) {
+printf("The number is even\n");
+} else {
+printf("The number is odd\n");
+}
+}
+
+int main() {
+int number;
+printf("Enter a number: ");
+scanf("%d", &number);
+checkEvenOrOdd(number);
+return 0;
+}
